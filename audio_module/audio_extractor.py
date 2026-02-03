e89bd50f3275ea4bbaf6ab52aa69233e5e4387af
hyperxt4 <uniyalabhinav54@gmail.com>
2026-02-03 23:40:31 +0530

    Added working audio defect detection code

--- audio_module/audio_extractor.py
+++ audio_module/audio_extractor.py
@@ -1,1 +1,9 @@
+from moviepy import VideoFileClip
 
+video_path = "input_video.mp4"
+audio_path = "extracted_audio.wav"
+
+video = VideoFileClip(video_path)
+video.audio.write_audiofile(audio_path)
+
+print("Audio extracted and saved as extracted_audio.wav")
